9ae6d6ffdb778292ccaa7342ca8571b20c8b4a90
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-05-08 18:07:49 +0600

    Create Login and Registration Form

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router'
+import auth from '../../firebase/firebase.init';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 const LogIn = () => {
+    const handleLogIn = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+    }
     return (
         // <div className='flex justify-center pt-30'>
         //     <div className="rounded-xl border border-gray-200 py-8 px-6 max-w-90 w-full">
@@ -39,11 +53,11 @@ const LogIn = () => {
 
                 <div className="w-full flex flex-col items-center justify-center">
 
-                    <form className="md:w-96 w-80 flex flex-col items-center justify-center">
+                    <form className="md:w-96 w-80 flex flex-col items-center justify-center" onSubmit={handleLogIn}>
                         <h2 className="text-4xl text-gray-900 font-medium">Sign in</h2>
                         <p className="text-sm text-gray-500/90 mt-3">Welcome back! Please sign in to continue</p>
 
-                        <button type="button" className="w-full mt-8 bg-gray-500/10 flex items-center justify-center h-12 rounded-full">
+                        <button type="button" className="btn w-full mt-8 bg-gray-500/10 flex items-center justify-center h-12 rounded-full">
                             <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/login/googleLogo.svg" alt="googleLogo" />
                         </button>
 
@@ -57,14 +71,14 @@ const LogIn = () => {
                             <svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M0 .55.571 0H15.43l.57.55v9.9l-.571.55H.57L0 10.45zm1.143 1.138V9.9h13.714V1.69l-6.503 4.8h-.697zM13.749 1.1H2.25L8 5.356z" fill="#6B7280" />
                             </svg>
-                            <input type="email" placeholder="Email" className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required />
+                            <input name='email' type="email" placeholder="Email" className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required />
                         </div>
 
                         <div className="flex items-center mt-6 w-full bg-transparent border border-gray-300/60 h-12 rounded-full overflow-hidden pl-6 gap-2">
                             <svg width="13" height="17" viewBox="0 0 13 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M13 8.5c0-.938-.729-1.7-1.625-1.7h-.812V4.25C10.563 1.907 8.74 0 6.5 0S2.438 1.907 2.438 4.25V6.8h-.813C.729 6.8 0 7.562 0 8.5v6.8c0 .938.729 1.7 1.625 1.7h9.75c.896 0 1.625-.762 1.625-1.7zM4.063 4.25c0-1.406 1.093-2.55 2.437-2.55s2.438 1.144 2.438 2.55V6.8H4.061z" fill="#6B7280" />
                             </svg>
-                            <input type="password" placeholder="Password" className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required />
+                            <input name='password' type="password" placeholder="Password" className="bg-transparent text-gray-500/80 placeholder-gray-500/80 outline-none text-sm w-full h-full" required />
                         </div>
 
                         <div className="w-full flex items-center justify-between mt-8 text-gray-500/80">
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,14 +1,29 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React from 'react';
 import { Link } from 'react-router';
+import auth from '../../firebase/firebase.init';
 const Register = () => {
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const userName = e.target.userName.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user)
+            })
+            .catch(error => {
+                console.log('Error:', error.message)
+            })
+    }
     return (
         <div className='flex justify-center pt-30'>
-            <form className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10">
+            <form onSubmit={handleRegister} className="bg-white text-gray-500 w-full max-w-[340px] mx-4 md:p-6 p-4 py-8 text-left text-sm rounded-lg shadow-[0px_0px_10px_0px] shadow-black/10">
                 <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Sign Up</h2>
 
-                <input id="email" className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3" type="text" placeholder="Username" required />
-                <input id="email" className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3" type="email" placeholder="Email" required />
-                <input id="email" className="w-full border mt-1 bg-indigo-500/5 mb-7 border-gray-500/10 outline-none rounded py-2.5 px-3" type="text" placeholder="Password" required />
+                <input name='userName' className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3" type="text" placeholder="Username" required />
+                <input name='email' className="w-full border mt-1 bg-indigo-500/5 mb-2 border-gray-500/10 outline-none rounded py-2.5 px-3" type="email" placeholder="Email" required />
+                <input name='password' className="w-full border mt-1 bg-indigo-500/5 mb-7 border-gray-500/10 outline-none rounded py-2.5 px-3" type="text" placeholder="Password" required />
 
                 <button className="w-full mb-3 bg-indigo-500 hover:bg-indigo-600 transition-all active:scale-95 py-2.5 rounded text-white font-medium">Create Account</button>
 
